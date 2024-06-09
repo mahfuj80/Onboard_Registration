4f4054d9694857f8996c2df23952722070f851f6
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-06-09 18:24:07 +0600

    added froms

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,241 @@
-<script setup></script>
+<script setup lang="ts">
+const items = ["1", "2", "3"];
+</script>
 
 <template>
-  <h1>This is index Page</h1>
-  <UIcon name="i-heroicons-bell-solid" />
-  <UButton
-    icon="i-heroicons-bell-solid"
-    size="md"
-    color="primary"
-    variant="solid"
-    label="sldk"
-  />
+  <section class="flex items-center justify-center min-h-screen py-12">
+    <UCarousel
+      :items="items"
+      :ui="{
+        item: 'basis-full',
+        container: 'rounded-lg',
+        indicators: {
+          wrapper: 'relative bottom-0 mt-4',
+        },
+      }"
+      arrows
+      indicators
+      class="w-80 mx-auto"
+    >
+      <template #default="{ item }">
+        <div
+          v-if="item === '1'"
+          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1
+              class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+            >
+              Sign in to your account
+            </h1>
+            <form class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Your email</label
+                >
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Password</label
+                >
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label
+                      for="remember"
+                      class="text-gray-500 dark:text-gray-300"
+                      >Remember me</label
+                    >
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >Forgot password?</a
+                >
+              </div>
+              <button
+                type="submit"
+                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?
+                <a
+                  href="#"
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >Sign up</a
+                >
+              </p>
+            </form>
+          </div>
+        </div>
+
+        <div
+          v-if="item === '2'"
+          class="flex items-center justify-center p-2 bg-white"
+        >
+          <!-- Author: FormBold Team -->
+          <div class="mx-auto w-full max-w-[550px] bg-white">
+            <form>
+              <div class="mb-5">
+                <label
+                  for="name"
+                  class="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Full Name"
+                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+              <div class="mb-5">
+                <label
+                  for="phone"
+                  class="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  placeholder="Enter your phone number"
+                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+
+              <div>
+                <button
+                  class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
+                >
+                  Book Appointment
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div
+          v-if="item === '3'"
+          class="max-w-md mx-auto mt-10 bg-white shadow-lg rounded-lg overflow-hidden"
+        >
+          <div
+            class="text-2xl py-4 px-6 bg-gray-900 text-white text-center font-bold uppercase"
+          >
+            Book an Appointment
+          </div>
+          <form class="py-4 px-6" action="" method="POST">
+            <div class="mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="name">
+                Name
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Enter your name"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="email">
+                Email
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="phone">
+                Phone Number
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone"
+                type="tel"
+                placeholder="Enter your phone number"
+              />
+            </div>
+
+            <div class="flex items-center justify-center mb-4">
+              <button
+                class="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Book Appointment
+              </button>
+            </div>
+          </form>
+        </div>
+      </template>
+
+      <template #indicator="{ onClick, page, active }">
+        <UButton
+          :label="String(page)"
+          :variant="active ? 'solid' : 'outline'"
+          size="2xs"
+          class="rounded-full min-w-6 justify-center"
+          @click="onClick(page)"
+        />
+      </template>
+
+      <template #prev="{ onClick, disabled }">
+        <button
+          :class="disabled ? 'hidden' : 'w-fit mx-auto'"
+          :disabled="disabled"
+          @click="onClick"
+        >
+          Prev
+        </button>
+      </template>
+
+      <template #next="{ onClick, disabled }">
+        <button
+          :class="disabled ? 'hidden' : 'w-fit mx-auto'"
+          :disabled="disabled"
+          @click="onClick"
+        >
+          Next
+        </button>
+      </template>
+    </UCarousel>
+  </section>
 </template>
