9a9f69c3117be1f5f0514d4607524972c0eca9de
unknown <shimul>
2024-06-22 15:36:29 +0600

    asdhfjkh3f

---
+++ components/homepage/header.vue
@@ -0,0 +1,7 @@
+<template>
+	<UContainer>Header</UContainer>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ pages/homepage/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<UContainer>Homepage</UContainer>
+</template>
+
+<script setup>
+	definePageMeta({
+		layout: "homepage",
+	});
+</script>
+
+<style scoped></style>
